0280b89c4fb09ee8cea7234c6ccc1eb6061669e6
swizzlefits <kyle.g.barker@gmail.com>
2025-02-15 18:13:14 -0800

    removed non-random generation in seeding

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def main():
     sys.setrecursionlimit(10000)
     win = Window(screen_x, screen_y)
 
-    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win, 10)
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
     print("maze created")
     is_solvable = maze.solve()
     if not is_solvable:
